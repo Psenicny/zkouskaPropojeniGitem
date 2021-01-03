10bc0d51e36dc8527951a0bc02df0f701e504873
Psenicny <jindrichpsenicny@gmail.com>
2021-01-03 14:22:35 +0100

    první commit v0.0.1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--Vytvoril: Jindrich Psenicny-->
+<html lang="cs-cz">
+<head>
+
+  <title>zkouška propojení Gitem</title>
+
+  <meta name="description" content="popis webu" />
+  <meta name="author" content="Jindrich Psenicny" />
+  <meta charset="utf-8" />
+  <meta name="google" content="notranslate" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+
+<body>
+
+0.0.1 /03.01.2021/
+  Tohle je zkouška úplně nového a prázdného projektu. Tady se můžeme učit přidávat prvky společně a zkoušet si je načítat.
+   začneme jednoduchými komentáři, které si označíme číslem a časovým razítkem. Potom si můžeme zkoušet vracet se v historii.
+
+</body>
+
+</html>
+
